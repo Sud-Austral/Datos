5004f910275ee4070d639da98a83407777ce8973
lmonsalve <lmonsalve22@gmail.com>
2020-04-05 23:00:11 -0400

    Update automatico via python

--- test.py
+++ test.py
@@ -28,3 +28,28 @@ def guardarRepositorio():
     origin.push()
     return True
 #************************************Actualizar repositorio***********************************************
+
+#************************************Actualizar Chile*****************************************************
+def Chile():
+    url_chile = "https://raw.githubusercontent.com/ivanMSC/COVID19_Chile/master/covid19_chile.csv"
+    pd.read_csv(url_chile).to_csv("Chile/covid19_chile.csv", index=False)
+    url_comunas = "https://raw.githubusercontent.com/ivanMSC/COVID19_Chile/master/covid19_comunas.csv"
+    pd.read_csv(url_chile).to_csv("Chile/covid19_comunas.csv", index=False)
+    url_old = "https://raw.githubusercontent.com/ivanMSC/COVID19_Chile/master/old/covid19_chile.csv"
+    pd.read_csv(url_chile).to_csv("Chile/covid19_chile_old.csv", index=False)    
+    data = pd.read_excel("covid19_chile.xlsx")
+    del data["Unnamed: 14"]
+    del data["Unnamed: 15"]
+    del data["Unnamed: 16"]
+    data["Fecha"] = data["Fecha"].apply(cambiaFecha)
+    data.to_csv("covid19_chile.csv", index=False)
+    
+    guardarRepositorio()    
+    return
+
+def cambiaFecha(texto):
+    if(type(texto) == datetime):
+        return texto.strftime("%m-%d-%Y")
+    else:
+        return texto
+#************************************Actualizar Chile*****************************************************
