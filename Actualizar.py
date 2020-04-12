f79841cf2c5d7940de4d592d88877764b552fd99
lmonsalve <lmonsalve22@gmail.com>
2020-04-12 16:29:26 -0400

    Update automatico via Actualizar 04-12-2020 16-29-26

--- Actualizar.py
+++ Actualizar.py
@@ -700,6 +700,43 @@ def minsal():
     return
 #************************************Actualizar Minsal*******************************************
 #************************************Actualizar BING NEWS*******************************************
+"""
+Argentina	AR
+Australia	AU
+Austria	AT
+Belgium	BE
+Brazil	BR
+Canada	CA
+Chile	CL
+Denmark	DK
+Finland	FI
+France	FR
+Germany	DE
+Hong Kong SAR	HK
+India	IN
+Indonesia	ID
+Italy	IT
+Japan	JP
+Korea	KR
+Malaysia	MY
+Mexico	MX
+Netherlands	NL
+New Zealand	NZ
+Norway	NO
+China	CN
+Poland	PL
+Portugal	PT
+Philippines	PH
+Russia	RU
+Saudi Arabia	SA
+South Africa	ZA
+Spain	ES
+Sweden	SE
+Switzerland	CH
+Taiwan	TW
+Turkey	TR
+United Kingdom	GB
+United States	US
 def bingNews():
     headers = {
         # Request headers
